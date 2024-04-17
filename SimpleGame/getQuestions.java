e4e55109b13ce321c6caff00118a860333ce306f
Shorez11 <166677219+Shorez11@users.noreply.github.com>
2024-04-16 19:56:30 -0500

    Update getQuestions.java

--- SimpleGame/getQuestions.java
+++ SimpleGame/getQuestions.java
@@ -3,6 +3,7 @@ package SimpleGame;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class getQuestions {
@@ -11,26 +12,46 @@ public class getQuestions {
         Random random = new Random();
         int randNum = random.nextInt(questionList.length);
         return randNum;
-        //Note: Can currently pull the same # again. Will write code in the future to get around this issue
     }
 	
 	public static String[][] questionList() throws Exception {
-		File file = new File("\\Users\\kjbet\\Questions.txt"); //Temp filepath 
+		File file = new File("/Users/shorez/Downloads/Questions.txt");//temp for file
 		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-			String[][] questions = new String[10][6]; 
-			// Number of columns and rows currently hard coded due to file.length basically being the # of characters in the file
-			//Want to see if there's an easier way, but after spending hours on dealing with randomaccessfile
-			//and having to scrap that, rn here's a simple solution
-			for (int i = 0; i < 10; i++) {
+			String[][] questions = new String[17][6]; //change the first number to amount of questions
+			
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			
+			for (int i = 0; i < 17; i++) { //change the amount of questions that will be put into the possible questions and creates random order
+				
 				String string = reader.readLine(); 
+				
 				String[] arrString = string.split(",", 0);
+				
+				int num = getRandomQuestion(questions);
+				
+				while(getFlag(list, num)) {
+					num = getRandomQuestion(questions);
+				}
+				
 				int j = 0;
+				
 					for (String a : arrString) {
-						questions[i][j] = a;
+						questions[num][j] = a;
 						j++;
 					}
+					
+				list.add(num);
 			}
 			return questions;
 		}
+	}
+	
+	public static boolean getFlag(ArrayList<Integer> list, int num) {
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i) == num) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
